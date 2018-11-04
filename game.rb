b4dab78c7c75a24f95f635096cb291abfb9c4c2c
Anja Istenic <aistenic1@gmail.com>
2018-11-04 19:11:54 +0100

    included user input and logicfor winning the game

--- game.rb
+++ game.rb
@@ -7,6 +7,18 @@ class Game < Gosu::Window
   WINDOW_WIDTH = 640
   WINDOW_HEIGHT = 480
 
+  GREEN = Gosu::Color.rgb(74, 219, 139)
+  RED = Gosu::Color.rgb(219, 4, 54)
+  LIGHT_BLUE = Gosu::Color.rgb(192, 229, 227)
+
+  GOSU_KB_VALUES = { 4 => 'A', 5 => 'B', 6 => 'C', 7 => 'D',
+                     8 => 'E', 9 => 'F', 10 => 'G', 11 => 'H',
+                     12 => 'I', 13 => 'J', 14 => 'K', 15 => 'L',
+                     16 => 'M', 17 => 'N', 18 => 'O', 19 => 'P',
+                     20 => 'Q', 21 => 'R', 22 => 'S', 23 => 'T',
+                     24 => 'U', 25 => 'V', 26 => 'W', 27 => 'X',
+                     28 => 'Y', 29 => 'Z' }.freeze
+
   def initialize
     super WINDOW_WIDTH, WINDOW_HEIGHT
     self.caption = 'Hangman: The Game'
@@ -16,30 +28,49 @@ class Game < Gosu::Window
     @font = Gosu::Font.new(self, 'Arial', 20)
     @font_big = Gosu::Font.new(self, 'Arial', 58)
     @game_over = false
-    @paused = false
+    @color = LIGHT_BLUE
+    @guessed = []
+    @wrong_guesses = 0
+  end
+
+  def guess
+    @user_guess = GOSU_KB_VALUES[@numeric_guess]
+    if @user_guess.nil?
+      @hidden = @password.hidden
+    elsif @password.word.include? @user_guess
+      @guessed << @user_guess
+      @color = GREEN
+      @hidden = @password.reveal_letter(@guessed)
+    else
+      @color = RED
+      @wrong_guesses += 1
+    end
+  end
+
+  def check_for_win
+    @win = !(@hidden.include? '_')
   end
 
   def update
-    unless @paused || @game_over
+    unless @game_over
+      guess
+      check_for_win
     end
   end
 
   def draw
+    @font.draw_text("Your guess: #{@user_guess}", 50, 150, 1, 1.0, 1.0, @color)
     @gallows.draw
-    # @password.draw
-    @font_big.draw_text(('_ ' * @password.length), 50, 55, 1, 1.0, 1.0, Gosu::Color.rgb(192, 229, 227))
-    @font_big.draw_text('PAUSED', 100, 100, 1, 1.0, 1.0, Gosu::Color::YELLOW) if @paused
-    @font_big.draw_text('GAME OVER', 100, 100, 1, 1.0, 1.0, Gosu::Color::RED) if @game_over
-  end
-
-  def toggle_paused
-    @paused = !@paused
+    @font_big.draw_text(@hidden, 50, 55, 1, 1.0, 1.0, LIGHT_BLUE)
+    @font_big.draw_text('You won!', 150, 200, 1, 1.0, 1.0, GREEN) if @win
+    @font_big.draw_text('Game over', 100, 100, 1, 1.0, 1.0, RED) if @game_over
   end
 
   def button_down(id)
-    case id
-    when Gosu::KbEscape then close
-    when Gosu::KB_P then toggle_paused
+    if id == Gosu::KbEscape
+      close
+    elsif GOSU_KB_VALUES.key?(id)
+      @numeric_guess = id.to_i
     end
   end
 end
--- word.rb
+++ word.rb
@@ -2,10 +2,10 @@ require 'gosu'
 require 'random-word'
 # generating word and game logic
 class Word
-  attr_reader :length
+  attr_reader :word, :hidden
   def initialize
     @word = generate_word
-    @length = @word.length
+    @hidden = hide_word
   end
 
   def generate_word
@@ -14,6 +14,14 @@ class Word
     a = RandomWord.nouns.next
     b = RandomWord.adjs.next
     word = [a, b]
-    word[i]
+    word[i].upcase
+  end
+
+  def hide_word
+    @hidden = @word.gsub(/[A-Z]/, '_ ')
+  end
+
+  def reveal_letter(arr)
+    @word.gsub(/[^#{arr.join}]/, '_ ')
   end
 end
